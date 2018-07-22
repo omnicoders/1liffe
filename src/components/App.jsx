15a98364e64e3d7992a233119f3621cceba09656
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-07-22 14:05:59 -0700

    add router and app level grid

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Router from './Router';
+import Footer from './Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Router className="Router" />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
