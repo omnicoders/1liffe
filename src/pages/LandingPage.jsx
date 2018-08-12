b78b099f479d9130b7b093c0d37c8a9b2eb48240
TS22082 <ts22082@gmail.com>
2018-08-12 14:41:31 -0700

    removed footer on smaller screens

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,7 +7,15 @@ class LandingPage extends Component {
       <div className="LandingPage">
         <div className="profile">
           <div>
-            <p>Ahmadeen Nassar</p>
+            <p>
+              <tab1>
+                Ahmadeen Nassar has been sending used books to his native land,
+                Ethiopia, for over 10 years. Through determination and tireless
+                work Amadeen has managed to establish 15 differnet libraries in
+                the region to help fight against a problem he sees as a major
+                issue blocking his people from opportunity. Illiteracy.
+              </tab1>
+            </p>
             <p>Founder</p>
           </div>
         </div>
